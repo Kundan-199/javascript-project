6651b05565298ab6380a38bed683945328c67c8b
Kundan-199 <kundanpatil1271999@gmail.com>
2024-07-04 08:33:26 +0000

    variables and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId =123334; // const keyword is used for only constant value this value cannot be change.
+let accountEmail ="abc@gmail.com"; 
+let accPass = "1232";
+city= "mumbai";
+
+//accountId=11123
+
+console.log( accountId); // show only single entity.
+
+console.table([accountId,accountEmail,accPass,city]);// show deta in tabular form
