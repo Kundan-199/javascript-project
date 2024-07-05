b60003b36a7afd82ede62e897a93b4d41f5bf22a
Kundan-199 <kundanpatil1271999@gmail.com>
2024-07-05 05:08:35 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+let name="kundan";
+let age= 25;
+let email ="kundan@gmail.com";
+
+//detatypes in javascript
+
+//number = is used for number value
+//string = is used for name or alfabetical value
+//bigint = is used for large number
+//boolean = is used for true or false
+// null = is used for null value
+//undefind = is used for undefined value for ex= let name; means name can not br assing the value thrn deta type is undefined
+ 
+
+// object
+
+console.log(typeof "name");
