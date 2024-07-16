ee3aba77b02a20c32848292a7aacf4fd3d799ba1
Kundan-199 <kundanpatil1271999@gmail.com>
2024-07-16 05:40:55 +0000

    userinput

---
+++ 01_basics/03_sumuser.js
@@ -0,0 +1,11 @@
+const num1= parseInt (prompt('enter the first number='));
+const num2= parseInt (prompt('Enter second number='));
+const sum = num1+num2;
+console.log("sum of ${num1} and ${num2} is ${sum}");
+
+//parseInt is used for convert the string data into number.
+// prompt is used for get a user input deta from user.
+//${num} is a directory to execute the program.
+//const is constant variable the value cannot be changed.
+// the program is not executed because prompt function is not reconized in node js.
+//it is not definer in node js so the program cannot run on node js envoirment.
